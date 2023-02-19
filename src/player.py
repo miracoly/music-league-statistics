1062cb0bed464ccbeaa27c5ff5488b71c584b140
miracoly <68049792+miracoly@users.noreply.github.com>
2023-02-19 15:16:10 +0100

    add function to get infos about myself

--- src/player.py
+++ src/player.py
@@ -3,7 +3,7 @@ import requests
 
 from dataclasses import dataclass
 from dacite import from_dict
-from typing import List, Dict
+from typing import List, Dict, Optional, TypeVar, Type
 from functional import seq
 from requests import RequestException
 
@@ -29,12 +29,49 @@ class _PlayerDto:
     user: _UserDto
 
 
+@dataclass
+class _MeDto:
+    chatToken: str
+    email: str
+    id: str
+    images: Dict
+    meta: Dict
+    name: str
+    stats: Dict
+
+@dataclass
+class Me:
+    id: str
+    name: str
+
+    T = TypeVar('T', bound='Me')
+
+    @classmethod
+    def new_from(cls: Type[T], dto: _MeDto) -> T:
+        return Me(dto.id, dto.name)
+
+
+def get_me(session_id: str) -> Optional[Me]:
+    json = _fetch_me(session_id)
+    return None if json is None else Me.new_from(from_dict(_MeDto, json))
+
+
 def get_members_of(league_id: str, session_id: str) -> List[Player]:
     json = _fetch_members_of(league_id, session_id)
     return seq(json) \
         .map(lambda entry: from_dict(_PlayerDto, entry)) \
         .map(lambda dto: Player(dto.user.id, dto.user.name))\
         .list()
+
+
+def _fetch_me(session_id: str) -> Optional[Dict]:
+    url = 'https://app.musicleague.com/api/v1/me'
+    headers = {'cookie': f'session={session_id}'}
+    try:
+        return requests.get(url, headers=headers).json()
+    except RequestException:
+        logging.error("Could not fetch infos about me from url %s", url)
+        return None
 
 
 def _fetch_members_of(league_id: str, session_id: str) -> List[Dict]:
--- tests/test_player.py
+++ tests/test_player.py
@@ -1,10 +1,24 @@
 import unittest
 
 from unittest.mock import patch, Mock
-from src.player import get_members_of, Player
+from src.player import get_members_of, Player, get_me, Me
 
 
 class TestPlayer(unittest.TestCase):
+
+    @patch('src.player._fetch_me')
+    def test_get_me__failure__return_none(self, _fetch_me_mock: Mock):
+        """Test get_me - error while fetching - return None"""
+        _fetch_me_mock.return_value = None
+        me = get_me("session-id")
+        self.assertEqual(None, me)
+
+    @patch('src.player._fetch_me')
+    def test_get_me__success__return_me(self, _fetch_me_mock: Mock):
+        """Test get_me - success - return me"""
+        _fetch_me_mock.return_value = fetched_me
+        me = get_me("session-id")
+        self.assertEqual(expected_me, me)
 
     @patch('src.player._fetch_members_of')
     def test_get_members_of__failure__return_empty_list(self, _fetch_members_of_mock: Mock):
@@ -21,6 +35,29 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(expected_players, players)
 
 
+expected_me = Me(id='5', name='Miracoly')
+
+fetched_me = {
+    "id": "5",
+    "name": "Miracoly",
+    "email": "bla@keks.com",
+    "images": {
+        "profile": "url-to-profile-picture"
+    },
+    "chatToken": "secret",
+    "meta": {
+        "adSegment": 4,
+        "isAdmin": False,
+        "patronTier": 0
+    },
+    "stats": {
+        "numLeagues": 5,
+        "numPlacedFirst": 1,
+        "numPlacedSecond": 0,
+        "numPlacedThird": 0
+    }
+}
+
 expected_players = [
     Player(id='1', name='rahel'),
     Player(id='2', name='fabian'),
@@ -31,27 +68,78 @@ expected_players = [
     Player(id='7', name='Seb')
 ]
 
-fetched_members = [{'created': '2022-06-07T06:18:16.594798Z', 'isAdmin': True, 'isPlayer': True,
-                    'user': {'id': '1', 'name': 'rahel',
-                             'profileImage': 'url-to-profile-picture'}},
-                   {'created': '2022-06-07T06:21:21.368052Z', 'isAdmin': False, 'isPlayer': True,
-                    'user': {'id': '2', 'name': 'fabian',
-                             'profileImage': 'url-to-profile-picture'}},
-                   {'created': '2022-06-07T06:24:02.572755Z', 'isAdmin': False, 'isPlayer': True,
-                    'user': {'id': '3', 'name': '1ɴɢ0ʟꜰ',
-                             'profileImage': 'url-to-profile-picture'}},
-                   {'created': '2022-06-07T06:35:24.583997Z', 'isAdmin': False, 'isPlayer': True,
-                    'user': {'id': '4', 'name': 'phil',
-                             'profileImage': 'url-to-profile-picture'}},
-                   {'created': '2022-06-07T07:07:33.892625Z', 'isAdmin': False, 'isPlayer': True,
-                    'user': {'id': '5', 'name': 'Miracoly',
-                             'profileImage': 'url-to-profile-picture'}},
-                   {'created': '2022-06-07T07:21:45.33194Z', 'isAdmin': False, 'isPlayer': True,
-                    'user': {'id': '6', 'name': 'Nils',
-                             'profileImage': 'url-to-profile-picture'}},
-                   {'created': '2022-06-07T07:48:34.627437Z', 'isAdmin': False, 'isPlayer': True,
-                    'user': {'id': '7', 'name': 'Seb',
-                             'profileImage': 'url-to-profile-picture'}}]
+fetched_members = [
+    {
+        "created": "2022-06-07T06:18:16.594798Z",
+        "isAdmin": True,
+        "isPlayer": True,
+        "user": {
+            "id": "1",
+            "name": "rahel",
+            "profileImage": "url-to-profile-picture"
+        }
+    },
+    {
+        "created": "2022-06-07T06:21:21.368052Z",
+        "isAdmin": False,
+        "isPlayer": True,
+        "user": {
+            "id": "2",
+            "name": "fabian",
+            "profileImage": "url-to-profile-picture"
+        }
+    },
+    {
+        "created": "2022-06-07T06:24:02.572755Z",
+        "isAdmin": False,
+        "isPlayer": True,
+        "user": {
+            "id": "3",
+            "name": "1ɴɢ0ʟꜰ",
+            "profileImage": "url-to-profile-picture"
+        }
+    },
+    {
+        "created": "2022-06-07T06:35:24.583997Z",
+        "isAdmin": False,
+        "isPlayer": True,
+        "user": {
+            "id": "4",
+            "name": "phil",
+            "profileImage": "url-to-profile-picture"
+        }
+    },
+    {
+        "created": "2022-06-07T07:07:33.892625Z",
+        "isAdmin": False,
+        "isPlayer": True,
+        "user": {
+            "id": "5",
+            "name": "Miracoly",
+            "profileImage": "url-to-profile-picture"
+        }
+    },
+    {
+        "created": "2022-06-07T07:21:45.33194Z",
+        "isAdmin": False,
+        "isPlayer": True,
+        "user": {
+            "id": "6",
+            "name": "Nils",
+            "profileImage": "url-to-profile-picture"
+        }
+    },
+    {
+        "created": "2022-06-07T07:48:34.627437Z",
+        "isAdmin": False,
+        "isPlayer": True,
+        "user": {
+            "id": "7",
+            "name": "Seb",
+            "profileImage": "url-to-profile-picture"
+        }
+    }
+]
 
 if __name__ == '__main__':
     unittest.main()
